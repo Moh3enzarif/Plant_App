9925c5b11c2cb2c1950a1bc59a9c141edbf12512
moh3n zarif <mohsenzarif70@gmail.com>
2024-07-18 16:10:57 +0330

    SearchBox And ListView Created

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:plant_app/const/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +10,107 @@ class HomePage extends StatefulWidget {
 }
 
 class _CartPageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  final List<String> _plantTypes = [
+    "|پیشنهادی|",
+    "|پرفروش‌ها",
+    "|پر‌بازدیدها",
+    "|گل‌های‌سمی",
+    "|گل‌گلخانه‌ای",
+  ];
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  width: size.width * 0.9,
+                  decoration: BoxDecoration(
+                    color: Constants.primaryColor.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.mic,
+                        color: Constants.textColor,
+                      ),
+                      const Expanded(
+                        child: Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: TextField(
+                            textAlign: TextAlign.start,
+                            showCursor: false,
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.only(
+                                  right: 5,
+                                ),
+                                hintText: "جستجو...",
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none),
+                            style: TextStyle(
+                              fontFamily: "Muli",
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Constants.textColor,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+              height: 70,
+              width: size.width,
+              child: ListView.builder(
+                reverse: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: _plantTypes.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Text(
+                        _plantTypes[index],
+                        style: TextStyle(
+                          fontFamily: "Muli",
+                          fontSize: 16,
+                          fontWeight: selectedIndex == index
+                              ? FontWeight.bold
+                              : FontWeight.w300,
+                          color: selectedIndex == index
+                              ? Constants.primaryColor
+                              : Constants.textColor,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
