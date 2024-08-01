8aa497cec59b0a29d4ae5e1318a81b47c241465a
moh3n zarif <mohsenzarif70@gmail.com>
2024-08-01 12:04:29 +0330

    overlayColor in Scaner

--- lib/screens/camera_page.dart
+++ lib/screens/camera_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:plant_app/screens/qr_overlay.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -12,15 +13,22 @@ class _CameraPageState extends State<CameraPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MobileScanner(
-        onDetect: (barcode) {
-          if (barcode.raw == null) {
-            debugPrint("Failed to Scan Barcode");
-          } else {
-            final code = barcode.raw!;
-            debugPrint("Barcode Found! $code");
-          }
-        },
+      body: Stack(
+        children: [
+          MobileScanner(
+            onDetect: (barcode) {
+              if (barcode.raw == null) {
+                debugPrint("Failed to Scan Barcode");
+              } else {
+                final code = barcode.raw!;
+                debugPrint("Barcode Found! $code");
+              }
+            },
+          ),
+          QRScannerOverlay(
+            overlayColor: Colors.white.withOpacity(0.5),
+          ),
+        ],
       ),
     );
   }
