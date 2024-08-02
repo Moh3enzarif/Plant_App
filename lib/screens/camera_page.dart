1cf1d382125d4f47a46a28f2f9bde30c667b1555
moh3n zarif <mohsenzarif70@gmail.com>
2024-08-02 12:13:00 +0330

    X button for Scaner Inserted

--- lib/screens/camera_page.dart
+++ lib/screens/camera_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:plant_app/screens/qr_overlay.dart';
+import 'package:plant_app/const/constants.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -10,6 +11,8 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  MobileScannerController controller = MobileScannerController();
+  // required options for the scanner
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +30,68 @@ class _CameraPageState extends State<CameraPage> {
           ),
           QRScannerOverlay(
             overlayColor: Colors.white.withOpacity(0.5),
+          ),
+          Positioned(
+            top: 50,
+            left: 20,
+            right: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  // X Button
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Constants.textColor.withOpacity(0.9),
+                    ),
+                    child: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                // Torch Button
+                // Container(
+                //   height: 40,
+                //   width: 40,
+                //   decoration: BoxDecoration(
+                //     borderRadius: BorderRadius.circular(50),
+                //     color: Constants.textColor.withOpacity(0.2),
+                //   ),
+                //   child: IconButton(
+                //     color: Colors.white,
+                //     icon: ValueListenableBuilder(
+                //       valueListenable: controller,
+                //       builder: (context, state, child) {
+                //         switch (state) {
+                //           // ignore: constant_pattern_never_matches_value_type
+                //           case TorchState.off:
+                //             return const Icon(
+                //               Icons.flash_off,
+                //               color: Colors.white,
+                //             );
+                //           // ignore: constant_pattern_never_matches_value_type
+                //           case TorchState.on:
+                //             return const Icon(
+                //               Icons.flash_on,
+                //               color: Colors.yellow,
+                //             );
+                //         }
+                //       },
+                //     ),
+                //     onPressed: () {
+                //       controller.toggleTorch();
+                //     },
+                //   ),
+                // ),
+              ],
+            ),
           ),
         ],
       ),
